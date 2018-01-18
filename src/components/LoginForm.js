e62c6e0df4efffb0f9f65f592ab21763dd2ec74c
yassimortensen <yassimortensen@gmail.com>
2018-01-18 13:55:43 -0500

    added create an account form in Login component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -9,7 +9,10 @@ class LoginForm extends React.Component{
 
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      create_name: '',
+      create_email: '',
+      create_password: ''
     }
   }
 
@@ -19,7 +22,12 @@ class LoginForm extends React.Component{
     })
   }
 
-  handleSubmit = (event) => {
+  handleNewAccount = (event) => {
+    event.preventDefault()
+    console.log(this.state)
+  }
+
+  handleLogin = (event) => {
     event.preventDefault()
     this.props.login(this.state.email, this.state.password)//.then(() => this.props.history.push('/goals'))
   }
@@ -27,14 +35,23 @@ class LoginForm extends React.Component{
   render(){
     // console.log(this.state)
     return(
-      <div style={{margin: '2%'}}>
-        <h4>Login Form</h4>
-        <form onSubmit={this.handleSubmit}>
-          Email: <input onChange={this.handleChange} name='email' type='text' value={this.state.email}/>
-          <br />
-          Password: <input onChange={this.handleChange} name='password' type='text' value={this.state.password} />
-          <br />
-          <input type='submit' />
+      <div style={{margin: '2%', textAlign: 'center'}}>
+        <div style={{display:'inline-block'}}>
+          <i className="material-icons w3-button w3-round-large" style={{display:'inline'}}>bubble_chart</i>
+          <h4 style={{display:'inline'}}>Welcome to Accountable</h4>
+        </div>
+        <h5>Track your goals to build a more positive life</h5>
+        <form onSubmit={this.handleNewAccount}>
+          <input onChange={this.handleChange} name='create_name' type='text' placeholder='Name' value={this.state.create_name} /><br />
+          <input onChange={this.handleChange} name='create_email' type='text' placeholder='Email' value={this.state.create_email} /><br />
+          <input onChange={this.handleChange} name='create_password' type='text' placeholder='Password' value={this.state.create_password} /> <br />
+          <input style={{marginTop:'1%'}} type='submit' value='Create an Account' />
+        </form>
+        <h5>OR</h5>
+        <form onSubmit={this.handleLogin}>
+          <input onChange={this.handleChange} name='email' type='text' placeholder='Email' value={this.state.email}/><br />
+          <input onChange={this.handleChange} name='password' type='text' placeholder='Password' value={this.state.password} /><br />
+          <input style={{marginTop:'1%'}} type='submit' value='Log in' />
         </form>
       </div>
     )
