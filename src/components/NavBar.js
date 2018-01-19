7f873a438b9782ec9f2c2f15c772c2f1c045a7cd
yassimortensen <yassimortensen@gmail.com>
2018-01-19 09:29:36 -0500

    fixed router glitch and added Logout functionality in NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
+import { connect } from "react-redux";
+import { logout } from '../actions/getAuthUser';
 
 class NavBar extends React.Component{
+
+  handleLogout = (event) => {
+    event.preventDefault()
+    this.props.logout()
+  }
+
   render(){
     return(
       <ul>
@@ -14,11 +22,11 @@ class NavBar extends React.Component{
         <div style={{width: '15%', textAlign: 'center', display: 'inline-block'}}>
           <li>{this.props.name}</li>
         </div>
-        <div style={{width: '10%', textAlign: 'right', display: 'inline-block'}}>
+        <div onClick={this.handleLogout} style={{width: '10%', textAlign: 'right', display: 'inline-block'}} className='w3-button w3-round-large'>
           <li>Log Out</li>
         </div>
       </ul>
     )
   }
 }
-export default withRouter(NavBar)
+export default withRouter(connect(null, { logout })(NavBar))
